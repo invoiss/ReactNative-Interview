e5d1ada839559f0198a702357ee641438cd5bd12
pouyarezvani <pouyarezvani10@gmail.com>
2021-09-09 16:43:39 -0700

    Refactor to Expo

--- App.tsx
+++ App.tsx
@@ -1,17 +1,23 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import MainTabScreen from './src/Navigation/Navigation';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
 
-const App = () => {
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
     return (
-        <NavigationContainer>
-            <MainTabScreen />
-        </NavigationContainer>
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
     );
-
+  }
 }
-export default App;
-
-
-
